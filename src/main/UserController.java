13ada746f14ce494636aaa684ab47ed7173ae177
edwingbd <edwingbd@gmail.com>
2018-12-05 17:38:59 -0600

    create the controller for the user

---
+++ src/main/UserController.java
@@ -0,0 +1,50 @@
+package main;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import common.DataAccess;
+import common.DataAccessImpl;
+
+public class UserController {
+	
+	private static UserController instance = new UserController();
+	
+	private DataAccess<String, User> dataAccess = new DataAccessImpl<String, User>();
+	
+	private UserController() {}
+	
+	public static UserController getInstance() { return instance; }
+	
+	public User addNewUser(User User) {
+		return dataAccess.add(User.getUsername(), User);
+	}
+	
+	public User getUserById(String id) {
+		return dataAccess.get(id);
+	}
+	
+	/*
+	public User updateUser(User User) {
+		return dataAccess.update(User.getId(), User);
+	}
+	*/
+	/*
+	public List<User> search(String query) {
+		String q = query.toLowerCase();
+		
+		return getAllUsers()
+				.stream()
+				.filter(User -> 
+					User.getId().startsWith(q) ||
+					User.getFirstName().toLowerCase().startsWith(q) ||
+					User.getLastName().toLowerCase().startsWith(q)
+				)
+				.collect(Collectors.toList());
+	}
+*/
+	public List<User> getAllUsers() {
+		return dataAccess.getAll();
+	}
+
+}
---
+++ src/main/UserData.java
@@ -0,0 +1,19 @@
+package main;
+
+import member.management.MemberController;
+
+public class UserData {
+	public static void load() {
+		UserController controller = UserController.getInstance();
+		User user1 = new User("Jymmy","12345678.",AccessLevel.ADMIN);
+		User user2 = new User("Bealfan","12345678.",AccessLevel.ADMIN);
+		User user3 = new User("Edwin","12345678.",AccessLevel.ADMIN);
+		User user4 = new User("Jesse","12345678.",AccessLevel.ADMIN);
+		User user5 = new User("Vorlea","12345678.",AccessLevel.ADMIN);
+		controller.addNewUser(user1);
+		controller.addNewUser(user2);
+		controller.addNewUser(user3);
+		controller.addNewUser(user4);
+		controller.addNewUser(user5);
+	}
+}
