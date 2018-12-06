2e7ad32810eadd17f51c35afaa3c20d46fa302ff
edwingbd <edwingbd@gmail.com>
2018-12-05 18:40:43 -0600

    updatate the user

--- src/main/LoginController.java
+++ src/main/LoginController.java
@@ -1,17 +1,39 @@
 package main;
 
+
+
+import common.LibMember;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class LoginController {
+	@FXML
+	TextField tfuser;
+	
+	
+	@FXML
+	TextField tfPassword;
+	
+	//private ObservableList<User> data = FXCollections.observableArrayList();
 	
 	@FXML
 	protected void baLogin(ActionEvent event) {
 		try {
+			//User =new user();
+			UserController controller = UserController.getInstance();
+			User userReview = controller.getUserByUsername("Jymmy");
+			
+			if (userReview != null)
+				System.out.println("paso"+userReview.getPassword());
+			else
+				System.out.println("no paso");
 			
 			Parent root = FXMLLoader.load(getClass().getResource("fxml_Options.fxml"));
 			Scene scene = new Scene(root,310,391);
